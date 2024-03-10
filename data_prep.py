d408efc6cc827889767eaa282c74b3d4a4d71324
Suchit G <suchitg04@gmail.com>
2024-03-10 19:23:33 +0530

    Add data prep steps to data_prep.py

--- data_prep.py
+++ data_prep.py
@@ -1,4 +1,5 @@
 import os
+import torch
 from sklearn.model_selection import train_test_split
 
 
@@ -13,19 +14,21 @@ class DataPreprocessor:
         with open(data_path, 'r') as file:
             self.data = file.read().splitlines()
 
+        self._prep_conversion()
+
     def split_data(self, train_split: float, dev_test_split: float):
         train_words, temp_words = train_test_split(self.data, train_size=train_split, random_state=42)
         dev_words, test_words = train_test_split(temp_words, test_size=dev_test_split, random_state=42)
 
-        return train_words, dev_words, test_words
+        return self._prep_xs_ys(train_words), self._prep_xs_ys(dev_words), self._prep_xs_ys(test_words)
 
-    def prepare_xs_ys(self):
-        chars = sorted(list(set(''.join(self.train_words))))
+    def _prep_conversion(self):
+        chars = sorted(list(set(''.join(self.data))))
 
-        char_to_int = {s: i + 1 for i, s in enumerate(chars)}
-        char_to_int['.'] = 0
+        self.char_to_int = {s: i + 1 for i, s in enumerate(chars)}
+        self.char_to_int['.'] = 0
         # int_to_char seems to be redundant. See if you can remove it safely
-        int_to_char = {i: s for s, i in char_to_int.items()}
+        self.int_to_char = {i: s for s, i in self.char_to_int.items()}
 
         two_chars = set()
         for c1 in chars + ["."]:
@@ -34,7 +37,19 @@ class DataPreprocessor:
 
         two_chars = sorted(list(two_chars))
 
-        char2_to_int = {s: i for i, s in enumerate(two_chars)}
-        int_to_char2 = {i: s for i, s in enumerate(two_chars)}
+        self.char2_to_int = {s: i for i, s in enumerate(two_chars)}
+        self.int_to_char2 = {i: s for i, s in enumerate(two_chars)}
 
-    
+    def _prep_xs_ys(self, word_set: list[str]):
+        xs = torch.tensor([], dtype=torch.long)
+        ys = torch.tensor([], dtype=torch.long)
+
+        for w in word_set:
+            chs = ["."] + list(w) + ["."]
+            for ch1, ch2, ch3 in zip(chs, chs[1:], chs[2:]):
+                ix1 = self.char2_to_int[ch1 + ch2]
+                ix2 = self.char_to_int[ch3]
+                xs = torch.cat((xs, torch.tensor([ix1])))
+                ys = torch.cat((ys, torch.tensor([ix2])))
+
+        return xs, ys
